ebabe31a38f5da1a10f578da4724900b0a7246d2
Ubuntu <dbwnsgur741@naver.com>
2020-02-28 05:39:01 +0000

    Updates

---
+++ precache-manifest.6eb83a41f5e36bbfec477b788edb1f0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb2ebddd21a28f2de4a47444661fb29",
+    "url": "/reactmovie/index.html"
+  },
+  {
+    "revision": "a3770ce36278dd955af7",
+    "url": "/reactmovie/static/js/2.89845bcc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/reactmovie/static/js/2.89845bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ced0ea6ea226d81bb22",
+    "url": "/reactmovie/static/js/main.46e08737.chunk.js"
+  },
+  {
+    "revision": "9dbb0dc6048fa4790b46",
+    "url": "/reactmovie/static/js/runtime-main.daeef3bc.js"
+  }
+]);
